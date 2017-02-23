992ed1adb4b435cc2b4993cef4687692b7f761e1
Stéphane Monfort <stephane.monfort@gmail.com>
2017-02-23 19:38:54 +0100

    Mise à jour object FileData.java

--- src/main/java/com/soprasteria/hashcode/dto/FileData.java
+++ src/main/java/com/soprasteria/hashcode/dto/FileData.java
@@ -8,11 +8,52 @@
  ***********************************************************************************************/
 package com.soprasteria.hashcode.dto;
 
+import java.util.List;
+
 /**
  * @author jdecure
  *
  */
 public class FileData {
 
-	
+	private List<Cache> caches;
+
+	private List<Endpoint> endpoints;
+
+	private List<Request> requests;
+
+	private List<Video> videos;
+
+	public List<Cache> getCaches() {
+		return caches;
+	}
+
+	public void setCaches(List<Cache> caches) {
+		this.caches = caches;
+	}
+
+	public List<Endpoint> getEndpoints() {
+		return endpoints;
+	}
+
+	public void setEndpoints(List<Endpoint> endpoints) {
+		this.endpoints = endpoints;
+	}
+
+	public List<Request> getRequests() {
+		return requests;
+	}
+
+	public void setRequests(List<Request> requests) {
+		this.requests = requests;
+	}
+
+	public List<Video> getVideos() {
+		return videos;
+	}
+
+	public void setVideos(List<Video> videos) {
+		this.videos = videos;
+	}
+
 }
