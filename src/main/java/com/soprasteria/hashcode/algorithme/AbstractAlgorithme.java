836025f1a7f93bcb98d358ca21bdb29ad499cd79
jdecure <jeremie.decure@soprasteria.com>
2017-02-23 20:53:32 +0100

    branchement avec le reader

--- src/main/java/com/soprasteria/hashcode/algorithme/AbstractAlgorithme.java
+++ src/main/java/com/soprasteria/hashcode/algorithme/AbstractAlgorithme.java
@@ -3,11 +3,12 @@ package com.soprasteria.hashcode.algorithme;
 import com.soprasteria.hashcode.dto.FileData;
 import com.soprasteria.hashcode.dto.output.SortieAlgorithme;
 import com.soprasteria.hashcode.dto.writer.FileWriter;
+import com.soprasteria.hashcode.reader.InputFileReader;
 
 public abstract class AbstractAlgorithme implements Algorithme {
 
 	private FileData lire(String entree) {
-		return null;
+		return InputFileReader.readFile(entree);
 	}
 
 	private void ecrire(SortieAlgorithme sortie, String nomFichier) {
