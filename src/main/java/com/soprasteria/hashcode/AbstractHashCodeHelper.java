fae9d0f0944954c75c7c109d1ac960c0605293da
Stéphane Monfort <stephane.monfort@gmail.com>
2017-02-23 18:43:31 +0100

    Partage pom

---
+++ src/main/java/com/soprasteria/hashcode/AbstractHashCodeHelper.java
@@ -0,0 +1,6 @@
+package com.soprasteria.hashcode;
+
+public class AbstractHashCodeHelper
+{
+
+}
---
+++ src/main/java/com/soprasteria/hashcode/exercice/MonExercice.java
@@ -0,0 +1,8 @@
+package com.soprasteria.hashcode.exercice;
+
+public class MonExercice {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
